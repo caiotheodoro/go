7b829ed1f5ad8a94dc4c935bd194b7bd7cfd66f0
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-08-24 20:55:25 -0300

    feat: 🎸 redis - docker config | service

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/caiotheodoro/go/application"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println(err)
 	}
+
 }
